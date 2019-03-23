9fd1edb66eab4463fa49e01809d1e56fefed28b1
SparksFullStack <csparks@antioch.edu>
2019-03-22 17:45:09 -0700

    first programmatic test running for CommentBox

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,3 +17,9 @@ it('renders a text area and a button', () => {
     expect(wrapper.find('textarea').length).toEqual(1);
     expect(wrapper.find('button').length).toEqual(1);
 });
+
+it('has a textarea that users can type in', () => {
+    wrapper.find('textarea').simulate('change', { target: { value: 'new comment' } });
+    wrapper.update();
+    expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+})
