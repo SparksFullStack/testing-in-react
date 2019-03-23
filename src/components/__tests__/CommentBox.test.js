02faff9c38c7340c4c9dd05444de0775239b21d6
SparksFullStack <csparks@antioch.edu>
2019-03-22 17:24:12 -0700

    added first round of testing for the comment box

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import CommentBox from '../CommentBox';
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = mount(<CommentBox />)
+})
+
+it('renders a text area and a button', () => {
+    expect(wrapper.find('textarea').length).toEqual(1);
+    expect(wrapper.find('button').length).toEqual(1);
+});
